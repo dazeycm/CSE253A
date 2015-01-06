cda8e32d8b88f2e8a1b8e4b23df1fee0d428ccd6
dazeycm <dazeycm@miamioh.edu>
2015-01-06 15:04:15 -0500

    more work on lab 3

--- Lab3.py
+++ Lab3.py
@@ -5,11 +5,25 @@ for i in list(range(1000)):
     randNums.append(random.randint(1, 10))
 print("There were a total of %d 5's" % (randNums.count(5)))
 
-dice = []
-yahtzee = False
-while(not yahtzee):
+count = 0
+while True:
+    dice = []
     for i in list(range(5)):
         dice.append(random.randint(1, 6))
-    
+    count += 1
+    if dice[0] == dice[1] == dice[2] == dice[3] == dice[4]:
+        break
+print("Got yahtzee on the %dth try" % (count))
+
+count = 0;
+for x in range(1000000):
+    dice = []
+    for i in list(range(5)):
+        dice.append(random.randint(1,6))
+    if dice[0] == dice[1] == dice[2] == dice[3] == dice[4]:
+        count += 1
+print(count/1000000)
+
+
     
 
