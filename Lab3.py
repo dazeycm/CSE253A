40af6bcd1445e7245696e67188aa237eadd46308
dazeycm <dazeycm@miamioh.edu>
2015-01-06 15:25:21 -0500

    finished lab3

--- Lab3.py
+++ Lab3.py
@@ -15,14 +15,27 @@ while True:
         break
 print("Got yahtzee on the %dth try" % (count))
 
-count = 0;
-for x in range(1000000):
-    dice = []
-    for i in list(range(5)):
-        dice.append(random.randint(1,6))
-    if dice[0] == dice[1] == dice[2] == dice[3] == dice[4]:
-        count += 1
-print(count/1000000)
+#===============================================================================
+# for x in range(1000000):
+#     dice = []
+#     for i in list(range(5)):
+#         dice.append(random.randint(1,6))
+#     if dice[0] == dice[1] == dice[2] == dice[3] == dice[4]:
+#         count += 1
+# print(count/1000000)
+#===============================================================================
+
+inputFile = open("weather.dat", "r")
+outputFile = open("stats.dat", "w")
+for line in inputFile:
+    data = line.split()
+    #list comprehensions: https://docs.python.org/2/tutorial/datastructures.html#list-comprehensions
+    data = [float(i) for i in data]
+    outputFile.write("Mininum: %.0f    Maximum: %.0f\n" % (min(data), max(data)))
+
+        
+
+
 
 
     
