02a0777d262199baaa3aff8b9a3eb1ab937573d0
Craig Dazey <dazeycm@miamioh.edu>
2015-01-07 00:02:23 -0500

    finished homework2

--- Homework2/Homework2Q2.py
+++ Homework2/Homework2Q2.py
@@ -1,3 +1,4 @@
+from audioop import avg
 inputFile = open("hw2.dat", "r")
 outputFile = open("calculatedScores.dat", "w")
 
@@ -9,4 +10,23 @@ for line in inputFile:
     quizScores = scores[:3]
     hwScores = scores[3:8]
     examScores = scores[8:]
+    
+    quiz = sum(quizScores)/len(quizScores)
+    hw = sum(hwScores)/len(hwScores)
+    exam = sum(examScores)/len(examScores)
+    
+    finalScore = quiz * .2 + hw * .3 + exam * .5
+    
+    if finalScore > 90:
+        letterGrade = 'A'
+    elif finalScore > 80:
+        letterGrade = 'B'
+    elif finalScore > 70:
+        letterGrade = 'C'
+    elif finalScore > 60:
+        letterGrade = 'D'
+    else:
+        letterGrade = 'F'
+        
+    outputFile.write("%20s %5.1f %1c\n" % (name, finalScore, letterGrade))
     
