d7404dc2aab202b96ffb32fee4629931125c2255
dazeycm <dazeycm@miamioh.edu>
2015-01-09 14:56:14 -0500

    added class notes to homework3

---
+++ Homework3/Homework3.py
@@ -0,0 +1,6 @@
+#dictionary where key is state code and value is list
+#set for entities
+#to get number of states an entity is in
+#   Go through set and see if it is in a value in the dictionary
+
+# for timezone stuff, key is time zone, value = list of states
