855785f875e4b9432c580b440cb4deab10381013
dazeycm <dazeycm@miamioh.edu>
2015-01-09 13:14:29 -0500

    correct exam answer

--- Exam1/exam.py
+++ Exam1/exam.py
@@ -16,17 +16,11 @@ for x in range(100):
 print(nums)
 print('%d numbers greater than 49' % count)
 
-#===============================================================================
-# date = input('Enter a date: ')
-# dateParts = []
-# if date.find('/') == -1:
-#     dateParts = date.split('-')
-# else:
-#     dateParts = date.split('/')
-# print('Month: %s\nDay: %s\nYear: %s\n' % (dateParts[0], dateParts[1], dateParts[2]))
-#===============================================================================
+
 date = input('Enter a date: ')
-month = date[0:2]
-day = date[3]
-year = date[5:]
-print('Month: %s\nDay: %s\nYear: %s\n' % (month, day, year))
+dateParts = []
+if date.find('/') == -1:
+    dateParts = date.split('-')
+else:
+    dateParts = date.split('/')
+print('Month: %s\nDay: %s\nYear: %s\n' % (dateParts[0], dateParts[1], dateParts[2]))
