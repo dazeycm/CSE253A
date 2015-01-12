c32afff055b08ce437068b61039aa40d492cbfb6
dazeycm <dazeycm@miamioh.edu>
2015-01-12 15:51:10 -0500

    most common entity and unique entity count done

--- Homework3/Homework3.py
+++ Homework3/Homework3.py
@@ -4,3 +4,44 @@
 #   Go through set and see if it is in a value in the dictionary
 
 # for timezone stuff, key is time zone, value = list of states
+
+def keyWithMaxValue(dict):
+    vals = list(dict.values())
+    keys = list(dict.keys())
+    return keys[vals.index(max(vals))]
+
+inputFile = open('US.dat', 'r')
+
+sanityCount = 0
+uniqueEntityNames = set()
+entityCount = {}
+statesWithEntities = {}
+
+for line in inputFile:
+    sanityCount += 1
+    print(sanityCount)
+    
+    line = line.split('\t')
+    entity = line[0]
+    state = line[1]
+    timeZone = line[4]
+    
+    uniqueEntityNames.add(entity)
+    
+    if entity not in entityCount:
+        entityCount[entity] = 1
+    else: 
+        entityCount[entity] += 1
+        
+    statesWithEntities[state] = [].append(entity)
+    
+print('Number of unique entities: %d' % len(uniqueEntityNames))
+key = keyWithMaxValue(entityCount)
+print('Entity with the most appearences: %s\nNumber of appearences: %d' % (key, entityCount[key])) 
+
+
+
+
+
+
+
