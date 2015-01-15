fcddf23241cb4cf13957ce16f9fcd5ec2e2f6b5c
dazeycm <dazeycm@miamioh.edu>
2015-01-15 17:13:48 -0500

    over it

--- Homework4/GUI.py
+++ Homework4/GUI.py
@@ -5,6 +5,7 @@ import string
 #radio button to decide if we want 0x or 0b in front of numbers
 
 root = Tk()
+root.geometry('250x225+30+30')
 root.title('Dumb Binary Conversions')
 titles = ['Binary to Decimal', 'Decimal to Binary', 'Hex to Decimal', 'Decimal to Hex'] 
 output = ['Fancy Ouput', 'Not Fancy Ouput']
@@ -13,19 +14,22 @@ def fancyPrint():
     print("%s to decimal: %s" % (lastEntered[0], int(btd, 2)))
     print("%s to binary: %s" % (lastEntered[1], bin(int(dtb))))
     print("%s to decimal: %s" % (lastEntered[2], int(htd, 16)))
-    print("%s to hex: %s\n" % (lastEntered[3], hex(int(dth))))
+    print("%s to hex: %s" % (lastEntered[3], hex(int(dth))))
+    print()
+    
     
 def boringPrint():
     print(int(btd, 2))
     print(bin(int(dtb)))
     print(int(htd, 16))
     print(hex(int(dth)))
+    print()
 
 def makeEntities(root):
     entities = []
     for i in range(len(titles)):
         row = Frame(root)
-        label = Label(row, width = 20, text = titles[i], anchor = 'w')
+        label = Label(row, width = 20, text = titles[i], fg = 'red', anchor = 'w')
         entry = Entry(row)
         label.grid(row = 0, column = 0)
         entry.grid(row = 0, column = 1)
@@ -38,8 +42,9 @@ def makeRadio(root):
     radio = IntVar()
     radio.set(0)
     count = 0
+    label = Label(root, width = 20, text = 'Select output style', fg = 'red').pack(anchor = 'center')
     for str in output:
-        r = Radiobutton(root, text = str, padx = 20, variable = radio, value = count).pack(anchor = W)
+        r = Radiobutton(root , text = str, padx = 20, variable = radio, value = count).pack(anchor = 'center')
         count += 1
     
 def compute():
@@ -51,6 +56,7 @@ def compute():
     htd = entities[2].get()
     dth = entities[3].get()
     
+    #minefield of error checking, but I think I got them all
     for str in (btd, htd, dtb, dth):
         for char in str:
             if char not in string.ascii_letters + string.digits:
@@ -63,7 +69,6 @@ def compute():
     elif any([int(i) < 0 or int(i) > 1 for i in btd]):
         showinfo('Input Error in binary to decimal ', 'Numbers must be between 0 and 1 inclusive')
         return   
-    
     
     if any([not i.isdigit() for i in dtb]):
         showinfo('Input Error in decimal to binary', 'Only enter digits')
@@ -107,11 +112,13 @@ entities = makeEntities(root)
 reset()
 makeRadio(root)
 
-b1 = Button(root, text = 'Compute', command = compute)
-b1.pack(side = LEFT, padx = 5, pady = 5)
-b2 = Button(root, text = 'Reset', command = reset)
-b2.pack(side = LEFT, padx = 5, pady = 5)
-b3 = Button(root, text = 'Quit', command = sys.exit)
-b3.pack(side = LEFT, padx = 5, pady = 5)
+row2 = Frame(root)
+b1 = Button(row2, text = 'Compute', command = compute)
+b1.grid(row = 1, column = 0)
+b2 = Button(row2, text = 'Reset', command = reset)
+b2.grid(row = 1, column = 1)
+b3 = Button(row2, text = 'Quit', command = sys.exit)
+b3.grid(row = 1, column = 2)
+row2.pack()
 
 root.mainloop()
