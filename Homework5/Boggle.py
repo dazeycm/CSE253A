356b567a84202bb7a3ef0fed7f51c01a4b128af5
Craig Dazey <dazeycm@miamioh.edu>
2015-01-19 00:31:17 -0500

    change button layout

--- Homework5/Boggle.py
+++ Homework5/Boggle.py
@@ -35,13 +35,13 @@ def fileCheck():
     
 def makeButtons():
     findFileButton = Button(root, text = 'Find File', command = fileCheck)
-    findFileButton.pack()
+    findFileButton.grid(row = 0, column = 0)
     startButton = Button(root, text = 'Start Game', command = playGame)
-    startButton.pack()
+    startButton.grid(row = 0, column = 1)
     endButton =  Button(root, text = 'End Game', command = quitGame)
-    endButton.pack()
+    endButton.grid(row = 0, column = 2)
     quitButton = Button(root, text = 'Quit', command = sys.exit)
-    quitButton.pack()
+    quitButton.grid(row = 0, column = 3)
     
 def playGame():
     sys.exit()
@@ -51,6 +51,7 @@ def quitGame():
 
 
 root = Tk()       
+root.geometry('300x300')
 makeButtons()
     
 dice = [['A','E','A','N','E','G'],['W','N','G','E','E','H'],['A','H','S','P','C','O'], ['L','N','H','N','R','Z'],['A','S','P','F','F','K'],['T','S','T','I','Y','D'],['O','B','J','O','A','B'],['O','W','T','O','A','T'],['I','O','T','M','U','C'],['E','R','T','T','Y','L'],['R','Y','V','D','E','L'],['T','O','E','S','S','I'],['L','R','E','I','X','D'],['T','E','R','W','H','V'],['E','I','U','N','E','S'],['N','U','I','H','M','Qu']]
