719d61ab9602c1eec9ce24aea32e6aa0fa145e2e
Craig Dazey <dazeycm@miamioh.edu>
2015-01-19 00:27:04 -0500

    added buttons for basic game functionality

--- Homework5/Boggle.py
+++ Homework5/Boggle.py
@@ -1,5 +1,5 @@
 from tkinter.filedialog import askopenfilename
-from tkinter.tix import Tk
+from tkinter import *
 
 def createFileOptions():
     options = {}
@@ -19,10 +19,46 @@ def openFile():
         for line in inputFile:
             if ' ' in line:
                 print('Invalid dictionary')
-                break
+                return False
             words.append(line)
+    if not all([words[i] <= words[i + 1] for i in range(len(words) - 1)]):
+        print('Invalid dictionary')
+        return False
+    return True
             
-root = Tk()           
+def fileCheck():
+    flag = openFile()
+    while not flag:
+        print('failure')
+        flag = openFile()
+    print('success')
+    
+def makeButtons():
+    findFileButton = Button(root, text = 'Find File', command = fileCheck)
+    findFileButton.pack()
+    startButton = Button(root, text = 'Start Game', command = playGame)
+    startButton.pack()
+    endButton =  Button(root, text = 'End Game', command = quitGame)
+    endButton.pack()
+    quitButton = Button(root, text = 'Quit', command = sys.exit)
+    quitButton.pack()
+    
+def playGame():
+    sys.exit()
+    
+def quitGame():
+    sys.exit()
+
+
+root = Tk()       
+makeButtons()
+    
 dice = [['A','E','A','N','E','G'],['W','N','G','E','E','H'],['A','H','S','P','C','O'], ['L','N','H','N','R','Z'],['A','S','P','F','F','K'],['T','S','T','I','Y','D'],['O','B','J','O','A','B'],['O','W','T','O','A','T'],['I','O','T','M','U','C'],['E','R','T','T','Y','L'],['R','Y','V','D','E','L'],['T','O','E','S','S','I'],['L','R','E','I','X','D'],['T','E','R','W','H','V'],['E','I','U','N','E','S'],['N','U','I','H','M','Qu']]
-openFile()
+
 root.mainloop()
+
+
+
+
+
+
